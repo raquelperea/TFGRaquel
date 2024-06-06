0a5f6b04ffc3ae24de8b648e7919b6134ddd0e71
Juan Perea <jperea@ipsvial.es>
2024-06-06 12:20:50 +0200

    add-binary lca.ico

--- dist.bat
+++ dist.bat
@@ -1,3 +1,3 @@
 @echo off
-rem pyinstaller --clean --onefile --windowed --add-data lca.ico:. --add-binary=lca.ico:. --icon lca.ico --splash lca.png  -d bootloader lca.py
+rem pyinstaller --clean --onefile --windowed --add-data lca.ico:. --add-binary lca.ico:. --icon lca.ico --splash lca.png  -d bootloader lca.py
 pyinstaller lca.spec
